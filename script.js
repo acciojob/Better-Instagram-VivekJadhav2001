0bfbb5b1960e945086f3eeccf39fc63a1a0e8072
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-09-19 20:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-//your code here
-const images = document.querySelectorAll(".image")
-        let draggedImage = null;
+const images = document.querySelectorAll(".image");
+    let draggedImage = null;
 
-        for (let i = 0; i < images.length; i++) {
-            images[i].addEventListener("dragstart", (e) => {
-                draggedImage = e.currentTarget
-            })
+    images.forEach(image => {
+      image.addEventListener("dragstart", (e) => {
+        draggedImage = e.currentTarget;
+      });
 
-            images[i].addEventListener("dragover", (e) => {
-                e.preventDefault()
-            })
+      image.addEventListener("dragover", (e) => {
+        e.preventDefault(); // allow drop
+      });
 
-            images[i].addEventListener("drop", (e) => {
+      image.addEventListener("drop", (e) => {
+        e.preventDefault();
+        const dropTarget = e.currentTarget;
 
-                const dropTarget = e.currentTarget
-                if (draggedImage.id !== dropTarget.id) {
-                    const imageForDraggedItem = getComputedStyle(draggedImage).backgroundImage
-                    const imageForDroppedItem = getComputedStyle(dropTarget).backgroundImage
+        if (draggedImage && draggedImage !== dropTarget) {
+          const draggedImgTag = draggedImage.querySelector("img");
+          const dropImgTag = dropTarget.querySelector("img");
 
-                    //Assign item from Dragged item to dropped Item
-                    dropTarget.style.backgroundImage = imageForDraggedItem
-
-                    // Assign image from dropped item to dragged item
-                    draggedImage.style.backgroundImage = imageForDroppedItem
-                }
-            })
+          // swap src attributes
+          const tempSrc = draggedImgTag.src;
+          draggedImgTag.src = dropImgTag.src;
+          dropImgTag.src = tempSrc;
         }
+      });
+    });
