e2910da3b91e3794fe705f42f4bba721bc1af56e
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-09-19 20:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+const images = document.querySelectorAll(".image")
+        let draggedImage = null;
+
+        for (let i = 0; i < images.length; i++) {
+            images[i].addEventListener("dragstart", (e) => {
+                draggedImage = e.currentTarget
+            })
+
+            images[i].addEventListener("dragover", (e) => {
+                e.preventDefault()
+            })
+
+            images[i].addEventListener("drop", (e) => {
+
+                const dropTarget = e.currentTarget
+                if (draggedImage.id !== dropTarget.id) {
+                    const imageForDraggedItem = getComputedStyle(draggedImage).backgroundImage
+                    const imageForDroppedItem = getComputedStyle(dropTarget).backgroundImage
+
+                    //Assign item from Dragged item to dropped Item
+                    dropTarget.style.backgroundImage = imageForDraggedItem
+
+                    // Assign image from dropped item to dragged item
+                    draggedImage.style.backgroundImage = imageForDroppedItem
+                }
+            })
+        }
