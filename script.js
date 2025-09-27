21a7e2a5da6fd66a9c963a31324b6313a0726945
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-09-27 18:19:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const images = document.querySelectorAll(".image");
-    let draggedImage = null;
+let draggedItem = null
+        const divs = document.querySelectorAll(".image")
 
-    images.forEach(image => {
-      image.addEventListener("dragstart", (e) => {
-        draggedImage = e.currentTarget;
-      });
+        for (let i = 0; i < divs.length; i++) {
 
-      image.addEventListener("dragover", (e) => {
-        e.preventDefault(); // allow drop
-      });
+            // Assign event dragstart to all divs/image-divs
+            divs[i].addEventListener("dragstart", (e) => {
+                draggedItem = e.currentTarget
+                // console.log(e, "start working")
+            })
 
-      image.addEventListener("drop", (e) => {
-        e.preventDefault();
-        const dropTarget = e.currentTarget;
+            // Assign event dragover to all divs/image-divs
+            divs[i].addEventListener("dragover", (e) => {
+                // console.log(e,"dragover is working")
+                e.preventDefault()
+            })
 
-        if (draggedImage && draggedImage !== dropTarget) {
-          const draggedImgTag = draggedImage.querySelector("img");
-          const dropImgTag = dropTarget.querySelector("img");
+            divs[i].addEventListener('drop', (e) => {
+                const dropTarget = e.currentTarget
+                console.log(e,"Drop")
+                if (draggedItem.id !== dropTarget.id) {
+                    // const imageForDraggedItem = draggedItem.style.backgroundImage
+                    // const imageForDroppedItem = dropTarget.backgroundImage
+                    const imageForDraggedItem = getComputedStyle(draggedItem).backgroundImage
+                    const imageForDroppedItem = getComputedStyle(dropTarget).backgroundImage
 
-          // swap src attributes
-          const tempSrc = draggedImgTag.src;
-          draggedImgTag.src = dropImgTag.src;
-          dropImgTag.src = tempSrc;
-        }
-      });
-    });
+                    //Assign item from Dragged item to dropped Item
+                    dropTarget.style.backgroundImage = imageForDraggedItem
+
+                    // Assign image from dropped item to dragged item
+                    draggedItem.style.backgroundImage = imageForDroppedItem
+                } 
+            })
+
+        } 
